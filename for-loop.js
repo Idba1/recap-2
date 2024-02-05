eedc94d7289085ec69b6dab2aff49598b5702eb0
Idba1 <moniraislamself@gmail.com>
2024-02-06 03:22:00 +0600

    for-loop

---
+++ for-loop.js
@@ -0,0 +1,16 @@
+for(let i = 0; i<=10 ; i++){
+    console.log("Assignment", i);
+}
+
+
+
+
+for (let j = 0; j<=100; j++){
+    console.log("Sorry", j);
+}
+
+
+const  x = [33, 55, 34, 78, 99, 24, 67, 78, 8];
+for (let k=0 ; k<x.length ; k++){
+    console.log(x[k]);
+}
