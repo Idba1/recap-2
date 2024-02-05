ca8c052a8523e01b5fc3a19d1ef79ab3b7c98c12
Idba1 <moniraislamself@gmail.com>
2024-02-06 05:15:08 +0600

    trim.js

---
+++ trim.js
@@ -0,0 +1,20 @@
+const x = "hello";
+const y = "hello ";
+if(x.trim() === y.trim()){
+    console.log("They both are same");
+}
+else{
+    console.log("They both are different");
+}
+
+
+
+
+/* const x = " hellO";
+const y = "Hello ";
+if(x.trim().toLowerCase() === y.trim().toLowerCase()){
+    console.log("They both are same");
+}
+else{
+    console.log("They both are different");
+} */
