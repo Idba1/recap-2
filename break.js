234b4cc1479ecf121364ef0f3d2dab8a6d8f4aa5
Idba1 <moniraislamself@gmail.com>
2024-02-06 04:50:41 +0600

    break

---
+++ break.js
@@ -0,0 +1,8 @@
+const numbers = [33, 55, 34, 78, 99, 24, 67, 78, 8];
+for(let number of numbers ){
+    console.log(number);
+    if(number === 78){
+        break;
+    }
+    // console.log(number);
+}
