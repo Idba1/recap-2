a98c4ef5542d9851623fe13ee70343e56a9d1ae1
Idba1 <moniraislamself@gmail.com>
2024-02-06 03:02:44 +0600

    while loop

---
+++ while_loop.js
@@ -0,0 +1,30 @@
+let i = 1;
+while(i<=10){
+    console.log("Mehedi Hasan Linkon", i);
+    i++;
+}
+
+
+
+
+
+// Sum
+let j = 0;
+let sum = 0;
+while(j<=5){
+    sum = sum + j;
+    j++;
+}
+console.log("Sum is",sum);
+
+
+
+
+// Multiply
+let k = 1;
+let multiply = 1;
+while(k<=5){
+    multiply = multiply * k;
+    console.log(multiply);
+    k++;
+}
