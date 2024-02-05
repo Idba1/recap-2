96b875b4f802ba1f1908902065599f458e9c994b
Idba1 <moniraislamself@gmail.com>
2024-02-06 05:08:14 +0600

    string

---
+++ string.js
@@ -0,0 +1,31 @@
+/* const x = "hello";
+const y = "Hello";
+if(x === y){
+    console.log("They both are same");
+}
+else{
+    console.log("They both are different");
+} */
+
+
+
+
+/* const x = "hello";
+const y = "Hello";
+if(x.toLowerCase() === y.toLowerCase()){
+    console.log("They both are same");
+}
+else{
+    console.log("They both are different");
+} */
+
+
+
+const x = "hello";
+const y = "Hello";
+if(x.toUpperCase() === y.toUpperCase()){
+    console.log("They both are same");
+}
+else{
+    console.log("They both are different");
+}
