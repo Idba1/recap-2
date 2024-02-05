2966d7addb3311c736fc40e81afb056d4ac7d751
Idba1 <moniraislamself@gmail.com>
2024-02-06 05:12:09 +0600

    includes.js

---
+++ includes.js
@@ -0,0 +1,20 @@
+/* const x = "hello world";
+const y = "Hellow";
+if(x.toLowerCase().includes(y.toLowerCase())){
+    console.log("exist");
+}
+else{
+    console.log("don't exist");
+} */
+
+
+
+
+const x = "hello world";
+const y = "Hello";
+if(x.toLowerCase().includes(y.toLowerCase())){
+    console.log("exist");
+}
+else{
+    console.log("don't exist");
+}
