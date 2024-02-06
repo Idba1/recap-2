ab5f3f3c05d707a0ff607042fa66806a4e23febf
Idba1 <moniraislamself@gmail.com>
2024-02-06 06:49:30 +0600

    templete.js

---
+++ templete.js
@@ -0,0 +1,16 @@
+const myPet = {
+    name : "Travis",
+    age : 2,
+    food : "Fish",
+    color : "White",
+    specis : "Hulo beral",
+    "is Male" : true,
+};
+
+/* const sentence = "my pet name is " +myPet.name + " His favourite food is " +myPet.food ;
+console.log(sentence); */
+
+
+
+const sentence = `my pet name is ${myPet.name}. His favourite food is ${myPet.food}`;
+console.log(sentence);
